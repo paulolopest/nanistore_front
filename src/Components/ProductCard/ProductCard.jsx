af9a7f52a8cd9a790d5a5ede4e4048cb32c0c0e3
paulolopest <paulotarsogl@gmail.com>
2024-07-16 19:58:22 -0300

    Pushing base project

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import './ProductCard.scss';
 import * as Icon from '@phosphor-icons/react';
+import { useNavigate } from 'react-router-dom';
 
 const ProductCard = () => {
+	const navigate = useNavigate();
+
+	const onClick = () => {
+		console.log('clicous');
+		navigate('/home');
+	};
+
 	return (
 		<div className="pdct-crd">
 			<div className="pdct-crd-fst-ctr">
 				<img src="https://acdn.mitiendanube.com/stores/001/111/914/products/bull-4979b7e878bc9b297217064851294929-1024-1024.webp" />
+				<span />
 			</div>
 
 			<div className="pdct-crd-scnd-crt">
 				<p className="pdct-crd-name">Shorts Compressão 2 em 1 - Shadow PB</p>
 
 				<div className="pdct-crd-price">
-					<div>
+					<div onClick={onClick}>
 						<span>R$ 90,00</span>
 						<h1>R$ 74,90</h1>
 						<p>Ou até 10x de R$ 08,50</p>
--- src/Pages/Main/MainPage.jsx
+++ src/Pages/Main/MainPage.jsx
@@ -7,33 +7,43 @@ import ProductCard from '../../Components/ProductCard/ProductCard';
 
 const MainPage = () => {
 	const { theme } = React.useContext(GlobalContext);
+	const [containerWidth, setContainerWidth] = React.useState({ left: 0, right: 0 });
+
+	const ref = React.useRef();
+
+	React.useEffect(() => {
+		if (ref.current) {
+			setContainerWidth({ left: -16 * 170 + 16 * 5, right: 0 });
+		}
+	}, [ref]);
+
+	console.log(containerWidth);
 
 	return (
 		<div className={`${theme && 'darkTheme'} mn-pg-ctr `}>
 			<div className="mn-pg-fstBanner">
 				<img src="https://acdn.mitiendanube.com/stores/001/111/914/themes/amazonas/slide-1710042654005-4772266774-4f3553d320cbed496b753698914a39091710042656.jpg?1121670904" />
 			</div>
-			<motion.div
-				className="mn-pg-crs"
-				drag
-				whileDrag={{ backgroundColor: 'red' }}
-				dragConstraints={{
-					top: -50,
-					left: -50,
-					right: 50,
-					bottom: 50,
-				}}
-
-				// dragConstraints={{ left: 0, right: 100 }}
-				// initial={{ opacity: 0, x: -50 }}
-				// whileInView={{ opacity: 1, x: 0 }}
-				// dragElastic={{ left: 0.1, right: 0.1 }}
-			>
-				<ProductCard />
-				<ProductCard />
-				<ProductCard />
-				<ProductCard />
-				<ProductCard />
+			<motion.div className="mn-pg-crs-ctr">
+				<motion.div
+					ref={ref}
+					className="mn-pg-crs"
+					drag="x"
+					dragConstraints={containerWidth}
+					dragElastic={0.2}
+				>
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+				</motion.div>
 			</motion.div>
 
 			<AnimatePresence
